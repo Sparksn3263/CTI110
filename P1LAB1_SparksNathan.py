c381ed7143fc7f358f30ffc028b4d848f35ff3bc
Sparksn3263 <sparksn3263@student.faytechcc.edu>
2026-02-21 14:26:30 -0600

    Add files via upload

---
+++ P1LAB1_SparksNathan.py
@@ -0,0 +1,9 @@
+# Nathan Sparks
+# 21 Feb 26
+# P1LAB1
+# Input and Print
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello,", first_name,last_name +"!", "Welcome to CTI-100.")
